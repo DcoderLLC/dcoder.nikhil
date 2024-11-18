c1d99b0a89fc334781b45b12e794ab84293bc6ec
DcoderLLC <dcoderllc@gmail.com>
2024-11-18 16:05:15 +0530

    Pages Routing done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
 import {Box} from "@mui/material"
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import './App.css'
+import Home from './Pages/Home';
+import Contact from './Pages/Contact';
+import Navbar from "./Components/NavBar/Navbar";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
 
   return (
     <Box>
-
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/about" element={<About />} /> */}
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </Router>
     </Box>
   )
 }
